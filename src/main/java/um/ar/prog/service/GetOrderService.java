5fb31c1ae2d2575841e825315897191792eef2a4
Julián Castillo <83615462+chuli99@users.noreply.github.com>
2023-11-15 11:44:25 -0300

    #1 getOrder service created

---
+++ src/main/java/um/ar/prog/service/GetOrderService.java
@@ -0,0 +1,13 @@
+package um.ar.prog.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class GetOrderService {
+
+    private final Logger log = LoggerFactory.getLogger(GetOrderService.class);
+}
